65b8b8db08c55e8515263e64d271146e1198c943
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-05-17 16:57:10 +0930

    Current progress

--- MathForGamesTask3/TankTurret.cpp
+++ MathForGamesTask3/TankTurret.cpp
@@ -1,5 +1,6 @@
 #include "TankTurret.h"
 
+
 void TankTurret::OnUpdate(float deltaTime)
 {
 	// Turret Rotation
@@ -14,4 +15,19 @@ void TankTurret::OnUpdate(float deltaTime)
 	}
 
 	Rotate(rotation * deltaTime);
+
+
+	// Turret shooting (WIP)
+	// TODO: Make it update each bullet when the tank turret's update function is called and the same with drawing
+	// Make it continue in the direction it was shot
+	// Set the location and rotation to be correct
+	// and probably more
+	if (IsKeyDown(KeyboardKey::KEY_SPACE)) { // space
+		raylib::Texture2D bulletSprite("res/bulletBlue2_outline.png");
+		SpriteObject Bullet;
+		Bullet.Sprite = &bulletSprite;
+		Bullet.SetLocalPosition(GetWorldPosition());
+		
+	}
+	
 }
