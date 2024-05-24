6ac52a5f7b345f55f9b99c88097588eb99ac9ad2
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-05-24 11:46:36 +0930

    Crate collision

--- MathForGamesTask3/MathForGamesTask3.cpp
+++ MathForGamesTask3/MathForGamesTask3.cpp
@@ -20,6 +20,7 @@ int main()
     raylib::Texture2D tankSprite("res/tankBody_blue_outline.png");
     raylib::Texture2D turretSprite("res/tankBlue_barrel2_outline.png");
     raylib::Texture2D crateSprite("res/crateMetal.png");
+    raylib::Texture2D bulletSprite("res/bulletBlue1_outline.png");
 
     // Creating the player
     TankPlayer Player;
@@ -47,15 +48,42 @@ int main()
         Player.Update(deltaTime);        
         Crate.Update(deltaTime);
 
+
+
+        // Collision checks
+        raylib::Rectangle crateRect(Crate.GetWorldPosition().x - Crate.Origin.x-27, Crate.GetWorldPosition().y - 27, crateSprite.width * Crate.GetWorldScale().x, crateSprite.height * Crate.GetWorldScale().y);
+        //raylib::Rectangle tankRect(Player.GetWorldPosition().x-42, Player.GetWorldPosition().y - 42, tankSprite.width * Player.GetWorldScale().x, tankSprite.height * Player.GetWorldScale().y);
+
+        //if (CheckCollisionRecs(crateRect, tankRect)) {
+        //     
+        //}
+        
+        for (int i = 0; i < 100; i++) {
+            if (PlayerTurret.bullets[i] != nullptr) {
+                raylib::Rectangle bulletRect(PlayerTurret.bullets[i]->GetWorldPosition().x - 12, PlayerTurret.bullets[i]->GetWorldPosition().y - 8, bulletSprite.width * PlayerTurret.bullets[i]->GetWorldScale().x, bulletSprite.height * PlayerTurret.bullets[i]->GetWorldScale().y);
+                if (CheckCollisionRecs(crateRect, bulletRect)) {
+                    delete PlayerTurret.bullets[i]->Sprite;
+                    PlayerTurret.bullets[i]->Sprite = nullptr;
+                    delete PlayerTurret.bullets[i];
+                    PlayerTurret.bullets[i] = nullptr;
+                }
+            }
+        }
+
+        // Drawing
         BeginDrawing();
         {
             window.ClearBackground(RAYWHITE);
-            Player.Draw();
-            PlayerTurret.Draw();
             Crate.Draw();
+            Player.Draw();
+
+            // Draws the turret & all existing bullets
+            PlayerTurret.Draw();
         }
         EndDrawing();
+        
 
+        
     }
     return 0;
 
