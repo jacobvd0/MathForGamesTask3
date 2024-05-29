b84e7b027107a2eaeecb3f656b06f7ac72a1207b
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-05-29 15:56:59 +0930

    working on boxes

---
+++ MathForGamesTask3/Box.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Vector3.h"
+
+using namespace math;
+
+class Box
+{
+	constexpr float min(float a, float b) { return a < b ? a : b; }
+	constexpr float max(float a, float b) { return a > b ? a : b; }
+
+	Vector3 min(const Vector3& a, const Vector3& b) {
+		return Vector3(min(a.x, b.x), min(a.y, b.y), 0);
+	}
+
+	Vector3 max(const Vector3& a, const Vector3& b) {
+		return Vector3(max(a.x, b.x), max(a.y, b.y), 0);
+	}
+
+	constexpr float clamp(float t, float a, float b) {
+		return max(a, min(b, t));
+	}
+	Vector3 clamp(const Vector3& t, const Vector3& a, const Vector3& b) {
+		return max(a, min(b, t));
+	}
+	
+};
+
