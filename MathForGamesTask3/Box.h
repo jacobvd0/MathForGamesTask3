d571c06a8d5271f943c4b40cca32438159342303
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-05-30 18:40:02 +0930

    Update Box.h

--- MathForGamesTask3/Box.h
+++ MathForGamesTask3/Box.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector3.h"
+#include <vector>
 
 using namespace math;
 
@@ -22,6 +23,49 @@ class Box
 	Vector3 clamp(const Vector3& t, const Vector3& a, const Vector3& b) {
 		return max(a, min(b, t));
 	}
+	
+	struct AABB {
+		AABB() {}
+		AABB(const Vector3& min, const Vector3& max) : min(min), max(max) {}
+
+		Vector3 min, max;
+
+
+		Vector3 Center() const {
+			return (min + max) * 0.5f;
+		}
+
+		std::vector<Vector3> Corners() const {
+			std::vector<Vector3> corners(4);
+
+			corners[0] = min;
+			corners[1] = { min.x, max.y, 0 };
+			corners[2] = max;
+			corners[3] = { max.x, min.y, 0 };
+		}
+
+		void fit(const Vector3* points, unsigned int count) {
+			min = {FLT_MAX, FLT_MAX, 0};
+			max = { FLT_MIN, FLT_MIN, 0 };
+			
+			for (unsigned int i = 0; i < count; i++, ++points) {
+				min = ::min(min, *points);
+				max = ::max(max, *points);
+			}
+
+
+		}
+
+		bool Overlaps(const Vector3& p) const
+		{
+			// test for not overlapped as it exits faster
+			return !(p.x < min.x || p.y < min.y ||
+				p.x > max.x || p.y > max.y);
+		}
+
+
+	};
+
 	
 };
 
